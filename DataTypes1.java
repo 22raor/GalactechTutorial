87ed393244c6f1aac6d963be50bb42cdc2522530
Rishi Rao <44213023+22raor@users.noreply.github.com>
2020-06-28 14:33:50 -0400

    Create DataTypes1

---
+++ DataTypes1.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String msg = "Hello World!";   //We initialize the variable here to "Hello World!"
+        System.out.println(msg);  //To use the value from the variable, we simply use the variable name, msg
+    }
+}
